ce0e48c1ff0bfdee2b877f41ba0c1a2b4c13ad25
Vaneyopasa <yvanessa27@outlook.com>
2022-09-15 14:37:28 -0500

    Ajuste de código footer y header

--- CarpetaFront/publicarME.js
+++ CarpetaFront/publicarME.js
@@ -6,4 +6,7 @@ function revisar() {
 
     if (condiciones !== condbase) {
         alert("Revisa que tus datos sean correctos antes de dar click en Enviar");
-    }}
+    }
+    else if(document.getElementById('condiciones').checked==true){
+        alert("Revisa que tus datos sean correctos antes de dar click en Enviar");
+    }} 
