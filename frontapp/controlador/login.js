eea473d430892e3ccfd3d2813326285f5f6f9176
Vaneyopasa <yvanessa27@outlook.com>
2022-09-28 09:24:47 -0500

    rutas hacia el index

--- frontapp/controlador/login.js
+++ frontapp/controlador/login.js
@@ -1,14 +1,30 @@
 
-function login(){
-    let user = document.loginform.user_name.value;
-    let password = document.loginform.pass_word.value;
-if(user == "" || pass == ""){
-      alert("Por favor ingrese un usuario y contraseña");
- }else{
-if((user == "ustut" || user == "USTUT") && password == "US@123"){
-      document.loginform.submit();
-}else{
-     alert("Please Enter Correct Credentials");
+
+// 
+
+
+function login() {
+    let correo = document.getElementById("loginEmail").value;
+    let password = document.getElementById("loginpsw").value;
+    
+    if (correo == "" || password == "") {
+        alert("Por favor ingrese un usuario y una contraseña");
+    } else if ((correo == "admin" || correo == "ADMIN") && password == "US@123") {
+        exitoso()      
+    }
 }
-   }
-       }
+
+
+function exitoso() {
+    const open = document.getElementById('open');
+    const modal_container = document.getElementById('modal_container');
+    const close = document.getElementById('close');
+
+    open.addEventListener('click', () => {
+        modal_container.classList.add('show');
+    });
+
+    close.addEventListener('click', () => {
+        modal_container.classList.remove('show');
+    });
+}
--- frontapp/controlador/registroUsuario.js
+++ frontapp/controlador/registroUsuario.js
@@ -1,1 +1,25 @@
 
+
+
+//Mostrar campos requeridos para la contraseña
+function myFocusFunction() {
+    document.getElementById("loginEmail").style.backgroundColor = "yellow";
+  }
+
+
+
+//revisar contraseña que contenga de 6 a 12 caracteres 
+function CheckPassword(inputtxt) 
+{ 
+let passw=  /^[A-Za-z]\w{6,12}$/;
+if(inputtxt.value.match(passw)) 
+{ 
+alert('Correct, try another...')
+return true;
+}
+else
+{ 
+alert('Wrong...!')
+return false;
+}
+}
