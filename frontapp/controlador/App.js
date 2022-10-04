7437d5844b270dfca3469b0ddc4215a827db29c9
Vaneyopasa <yvanessa27@outlook.com>
2022-10-04 16:27:54 -0500

    json y js

--- frontapp/controlador/App.js
+++ frontapp/controlador/App.js
@@ -1,8 +1,8 @@
 
 
 // ya funciona la conexion de input 1 creando una alerta con el contenido que este en esta.
-function boton1 (){
+/* function boton1 (){
     let nombre_mascota = document.getElementById('nombrem').value;
     alert(nombre_mascota,'hola');
-}
+} */
 
--- frontapp/controlador/verPublicaciones.js
+++ frontapp/controlador/verPublicaciones.js
@@ -1,4 +1,66 @@
 
+
+//evento de carga de la página
+//codigo para recorre mascotas y consumir api
+document.addEventListener("DOMContentLoaded",() => {
+
+  const mascotasContainer = document.querySelector("#mascotasContainer");
+
+
+fetch('./data/Mascotas.json')
+
+.then(response => response.json())
+
+.then(data => {
+  data.array.forEach(mascota => {
+  mascotasContainer.innerHTML +=`
+  <div class="col-md-3 publicacion" id="mascotasContainer">
+  <div class="card text-center flip-card">
+      <img id="myImg" src="./Assets/${mascota.id}" alt="Contacto: 3107729052" class="card-img-top imgpublication">
+
+      <div class="card-body bg-opacity-10 bg-black">
+
+          <button title="ver más información" id="btnInformacion" class="btnpublicacion" onclick="seeMore()"><img
+                  src="img/informacionPubli.png" class=" card-footer iconosFoto" ></button>
+
+          <button title="Editar publicación" id="btnEditar" class="btnpublicacion"onClick="editPost(this)" ><img
+                  src="img/editarPubli.png" class=" card-footer iconosFoto " ></button>
+
+          <button title="Eliminar publicación" id="btnEliminar" class="btnpublicacion" onClick="deletePost(this)"><span><img
+                      src="img/eliminarPubli.png" class=" card-footer iconosFoto" ></span></button>
+      </div>
+      <!-- The Modal to zoom image -->
+      <div id="myModal" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close">&times;</span>
+
+          <!-- Modal Content (The Image) -->
+          <img class="modal-content" id="img01">
+
+          <!-- Modal Caption (Image Text) -->
+          <div id="caption"></div>
+      </div>
+      <div id="myDIV1" class="moreInfo">
+          <h6 class="title">${mascota.nombreMascota}</h6>
+          <p class="caracteristica">Raza: <span class="mascotaDatos" id="razaInfo">${mascota.raza}</span></p>
+          <p class="caracteristica">Sexo: <span class="mascotaDatos" id="sexoInfo">${mascota.sexo}</span></p>
+          <p class="caracteristica">Estado: <span class="mascotaDatos" id="ciudadInfo">${mascota.estado}</span></p>
+          <p class="caracteristica" id="parrafo">Especie<span class="mascotaDatos" id="especieInfo">${mascota.descripcion}</span></p>
+      </div>
+  </div>
+  `
+    
+  });
+  console.log(data);
+})
+})
+
+
+
+
+
+
 //CODIGO PARA EL ZOOM DE LA IMAGEN
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -24,28 +86,17 @@ span.onclick = function() {
 
 
 
+
+
+
 //CODIGO PARA MOSTRAR INFORMACION
 function seeMore() {
-  let publicacion1 = document.getElementById("myDIV");
-  let publicacion2 = document.getElementById("myDIV2");
-  let publicacion3 = document.getElementById("myDIV3");
+  let myDIV = document.getElementById("myDIV1");
 
-  if (publicacion1.style.display === "block") {
-    publicacion1.style.display = "none";
+  if (myDIV.style.display === "block") {
+    myDIV.style.display = "none";
   } else {
-    publicacion1.style.display = "block";
-  }
-
-  if (publicacion2.style.display === "block") {
-    publicacion2.style.display = "none";
-  } else {
-    publicacion2.style.display = "block";
-  }
-
-  if (publicacion3.style.display === "block") {
-    publicacion3.style.display = "none";
-  } else {
-    publicacion3.style.display = "block";
+    myDIV.style.display = "block";
   }
 }
 
@@ -64,4 +115,7 @@ function seeMore() {
 
 
 //CODIGO PARA EL FILTRO
+
+
+
 
